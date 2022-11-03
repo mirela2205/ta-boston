f104700a289b04607af7d81d7b204cbbae7b0c45
mirela2205 <mirela.dina88@gmail.com>
2022-11-03 19:58:16 +0200

    758

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
       hello boston <i className="fa-solid fa-house"></i>
+      <img src="/images/logo.jpg" alt="Alt"></img>
     </div>
   );
 }
